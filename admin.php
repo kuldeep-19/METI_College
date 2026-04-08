732d776a49e7de18aaca95fa322a26059277b9e3
Kuldeep Gupta <kuldeepgdeoria@gmail.com>
2026-04-08 13:04:41 +0530

    testing

--- admin.php
+++ admin.php
@@ -83,8 +83,8 @@ if(isset($_GET['edit'])){
 <head>
     <title>Slider Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-
+</head> 
+   
 <body class="p-4">
 
     <div class="container">
